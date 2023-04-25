6c86b0510afc223f364b58ecda1eaca158b1a57b
kishashh <48453359+kishashh@users.noreply.github.com>
2023-04-25 09:27:08 -0500

    Removed "Continue as Guest" button
    Guest button removed, only google users are allowed to play chess :)

--- playchess.py
+++ playchess.py
@@ -11,8 +11,8 @@ class ChessGUI(QMainWindow):
 
         self.label = QLabel("Welcome to chess!")
         self.label.setFont(QFont("Arial", 20))
-        self.button_guest = QPushButton("Play as Guest")
-        self.button_guest.clicked.connect(self.on_guest_button_click) # Connect the guest button click event to the on_guest_button_click function
+        # self.button_guest = QPushButton("Play as Guest")
+        # self.button_guest.clicked.connect(self.on_guest_button_click) # Connect the guest button click event to the on_guest_button_click function
         self.button_signin = QPushButton("Sign in")
         self.button_signin.clicked.connect(self.on_signin_button_click) # Connect the sign in button click event to the on_signin_button_click function
 
@@ -22,7 +22,7 @@ class ChessGUI(QMainWindow):
         right_layout = QVBoxLayout()
         right_layout.addWidget(self.label)
         right_layout.addStretch(1)
-        right_layout.addWidget(self.button_guest)
+        # right_layout.addWidget(self.button_guest)
         right_layout.addWidget(self.button_signin)
         layout.addLayout(right_layout)
 
@@ -31,14 +31,14 @@ class ChessGUI(QMainWindow):
         main_widget.setLayout(layout)
         self.setCentralWidget(main_widget)
 
-    def on_guest_button_click(self):
-        # Open the chess_gui.py file using subprocess
-        subprocess.Popen(["python", "chess_gui.py"])
-        self.close()
+    # def on_guest_button_click(self):
+    #     # Open the chess_gui.py file using subprocess
+    #     subprocess.Popen(["python", "chess_gui.py"])
+    #     self.close()
 
     def on_signin_button_click(self):
         # Open the signin_gui.py file using subprocess
-        subprocess.Popen(["python", "signin_gui.py"])
+        subprocess.Popen(["python", "SignIn.py"])
         self.close()
 
 if __name__ == '__main__':
