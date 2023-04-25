e744d04a83d7c57ba0d96ce12d69d2ac89aae6b0
CollierDalton <collierjdalton@gmail.com>
2023-04-25 13:04:42 -0500

    Added different colored pieces
    Updated enums.py and chess_engine.py Tried to add different colored pieces. Would result in a bug and would have the game automatically won by black team.
    
    I reverted back to where the game compiles and runs, but decided to keep the names of the changed pieces.

--- enums.py
+++ enums.py
@@ -1,8 +1,11 @@
 class Player:
+    #PLAYER_1 = {'white', 'green'}
     PLAYER_1 = 'white'
     PLAYER_2 = 'black'
-    PLAYER_3 = 'blue'
+    PLAYER_3 = 'green'
     PLAYER_4 = 'red'
     EMPTY = -9
-    PIECES = ['blue_r', 'white_r', 'white_n', 'white_b', 'white_q', 'white_k', 'white_p',
+    PIECES = ['green_r', 'green_n', 'green_b', 'green_q', 'green_p', 'white_r', 'white_n', 'white_b', 'white_q', 
+              'white_k', 'white_p', 'red_r', 'red_n', 'red_b', 'red_q', 'red_p',
               'black_r', 'black_n', 'black_b', 'black_q', 'black_k', 'black_p']
+    
