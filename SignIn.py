bfdbc50e4257062a828d26ea9ec163a814680630
Crisco8828 <crisco.dalton@yahoo.com>
2023-04-25 01:02:18 -0500

    Added workin link in SignIn.py
    I added a link to the Google Sign in Page in the comments

--- SignIn.py
+++ SignIn.py
@@ -7,10 +7,14 @@ CLIENT_ID = '1035421752277-7vme3emqji5423fv1s09b64nvam2pfi3.apps.googleuserconte
 
 app = Flask(__name__)
 
+# Link to Google Sign In Page
+# https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&client_id=1035421752277-7vme3emqji5423fv1s09b64nvam2pfi3.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&service=lso&o2v=1&flowName=GeneralOAuthFlow
+
 @app.route('/')
 def verify_token():
     # token = request.form['https://oauth2.googleapis.com/token']
-    token = request.args.get('token', '')
+    # token = request.args.get('token', '')
+    token = request.args.get('http://127.0.0.1:5000/token')
     user_info = verify_google_token(token)
     if user_info is not None:
         user_id, email = user_info
